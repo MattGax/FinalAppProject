9a49231558fc9d3df0d672ea8238ffe09a1b4c6f
Matthew Burris <060216@swcsd.us>
2021-04-19 11:26:24 -0400

    First Commit

--- FinalAppProject/ViewController.swift
+++ FinalAppProject/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Matthew Burris on 4/19/21.
 //  Copyright © 2021 Matthew Burris. All rights reserved.
 //
+//  Planning DOC at https://www.notion.so/Academic-App-Plan-5ea52d6af08e499f97ddc3ed374712d6
 
 import UIKit
 
@@ -12,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
